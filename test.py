fd32e832385787ceb1b44ba1c60bb86a7ca52e6e
omgimanerd <alvin@omgimanerd.tech>
2018-11-29 15:29:04 -0500

    test

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 # Author: axl1439
 
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 import numpy as np
 import os
 
@@ -14,31 +16,47 @@ EYE_CASCADE = cv2.CascadeClassifier(
 FACE_CASCADE = cv2.CascadeClassifier(
     os.path.join(BASE_DIR, 'haarcascade_frontalface_default.xml'))
 
+
+# integral_image = cv2.integral(frame)
+# cv2.imshow('Integral', integral_image)
+
+def test_webcam():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print('Error opening webcam')
+        return
+    while cap.isOpened():
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = FACE_CASCADE.detectMultiScale(gray)
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(1)
+
 def main():
     cap = cv2.VideoCapture(os.path.join(BASE_DIR, 'data/face2.mp4'))
     # cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print('Error reading video')
-    while cap.isOpened():
+    subplot = plt.subplot()
+    window = []  
+    def update(i):
         ret, frame = cap.read()
-        if not ret:
-            break
-        cv2.imshow('Frame', frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = FACE_CASCADE.detectMultiScale(gray)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame, (x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-        cv2.imshow('p', frame)
-        # green = frame[:,:,1]
-        # cv2.imshow('G', green)
-        # integral_image = cv2.integral(frame)
-        # cv2.imshow('Integral', integral_image)
-        if (cv2.waitKey(0) & 255) == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face = frame[y:y + h, x:x + w]
+            green = np.sum(face[:,:,1])
+            window.append(np.sum(face[:,:,1]))
+            subplot.plot(np.arange(len(window)), window, 'b')
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(1)
+    
+    animation = anim.FuncAnimation(plt.gcf(), update, interval=16)
+    plt.show()
 
 if __name__ == '__main__':
+    # test_webcam()
     main()
